dc73b94f5770783da82582ad9d5a991874b1908d
EmmanuelDuarteClaudino <64570786+EmmanuelDuarteClaudino@users.noreply.github.com>
2021-01-15 14:44:33 -0300

    update main file
    bug fix in victory verification, on tie verification

--- jogoVelha.c
+++ jogoVelha.c
@@ -9,31 +9,31 @@
 */
 int verificarVitoria(char tabela [3][3], char simbolo, int numeroJogadas){
     if ((tabela [0][0]) == (tabela [0][1]) && (tabela [0][0]) == (tabela [0][2])){
-        printf("O jogador que utilizou o simbolo %c ganhou!!!!\n", simbolo);
+        printf(" O jogador que utilizou o simbolo %c ganhou!!!!\n\n", simbolo);
         return 0;
     } else if ((tabela [1][0]) == (tabela [1][1]) && (tabela [1][0]) == (tabela [1][2])){
-        printf("O jogador que utilizou o simbolo %c ganhou!!!!\n", simbolo);
+        printf(" O jogador que utilizou o simbolo %c ganhou!!!!\n\n", simbolo);
         return 0;
     } else if ((tabela [2][0]) == (tabela [2][1]) && (tabela [2][0]) == (tabela [2][2])){
-        printf("O jogador que utilizou o simbolo %c ganhou!!!!\n", simbolo);
+        printf(" O jogador que utilizou o simbolo %c ganhou!!!!\n\n", simbolo);
         return 0;
     } else if ((tabela [0][0]) == (tabela [1][0]) && (tabela [0][0]) == (tabela [2][0])){
-        printf("O jogador que utilizou o simbolo %c ganhou!!!!\n", simbolo);
+        printf(" O jogador que utilizou o simbolo %c ganhou!!!!\n\n", simbolo);
         return 0;
     } else if ((tabela [0][1]) == (tabela [1][1]) && (tabela [0][1]) == (tabela [2][1])){
-        printf("O jogador que utilizou o simbolo %c ganhou!!!!\n", simbolo);
+        printf(" O jogador que utilizou o simbolo %c ganhou!!!!\n\n", simbolo);
         return 0;
     } else if ((tabela [0][2]) == (tabela [1][2]) && (tabela [0][2]) == (tabela [2][2])){
-        printf("O jogador que utilizou o simbolo %c ganhou!!!!\n", simbolo);
+        printf(" O jogador que utilizou o simbolo %c ganhou!!!!\n\n", simbolo);
         return 0;
     } else if ((tabela [0][0]) == (tabela [1][1]) && (tabela [0][0]) == (tabela [2][2])){
-        printf("O jogador que utilizou o simbolo %c ganhou!!!!\n", simbolo);
+        printf(" O jogador que utilizou o simbolo %c ganhou!!!!\n\n", simbolo);
         return 0;
     } else if ((tabela [0][2]) == (tabela [1][1]) && (tabela [0][2]) == (tabela [2][0])){
-        printf("O jogador que utilizou o simbolo %c ganhou!!!!\n", simbolo);
+        printf(" O jogador que utilizou o simbolo %c ganhou!!!!\n\n", simbolo);
         return 0;
     } else if (numeroJogadas == 9){
-        printf("A partida deu velha(empatou) :(\n");
+        printf(" A partida deu velha(empatou) :(\n\n");
         return 0;        
     }
 }
@@ -61,7 +61,7 @@ int main (){
     char tabela [3][3];
     int jogando = 1;
     int jogou = 0;
-    int numeroJogadas = 1;
+    int numeroJogadas = 0;
     int condicionalPosicao = 0;
     char simbolo = 'X';
     char posicao;
@@ -74,15 +74,14 @@ int main (){
         }else if ( i == 2){
             condicionalPosicao = 9;
         }
-        for (register int j = 0; j < 3; j++){
+        for (register int j = 0; j < 3; j++, condicionalPosicao -= 3){
             tabela [i][j] = condicionalPosicao+'0';
-            condicionalPosicao -= 3;
         }  
     } 
-    printf(" Jogo da velha 1.0v \n Emmanuel Duarte Claudino GPL3\n\n");
+    printf(" Jogo da velha 1.1v \n Emmanuel Duarte Claudino - GPL3\n\n");
     while (jogando){
         mostrarTabela(tabela);
-        printf("\n Qual posicao voce quer jogar?");
+        printf("\n Qual posicao voce quer jogar? ");
         scanf("%c", &posicao);
         printf("\n");
         while ((getchar()) != '\n');
@@ -145,16 +144,17 @@ int main (){
                 printf("A posicao %c nao existe ou esta incorreta, tente novamente\n\n", posicao);
                 break;
             }
-        jogando = verificarVitoria(tabela, simbolo, numeroJogadas);
         if (jogou){
             jogou = 0;
-            numeroJogadas++;
+            ++numeroJogadas;
+            jogando = verificarVitoria(tabela, simbolo, numeroJogadas);
             if ( simbolo == 'X' ){
                 simbolo = 'O';
             }else{
                 simbolo = 'X';
             }
         }
+        
            
     }
     mostrarTabela(tabela);
